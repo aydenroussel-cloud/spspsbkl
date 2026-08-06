f1d4adfb3352b1be929206f25890f9ccb354692a
aydenroussel-cloud <aydenroussel@gmail.com>
2026-08-06 13:51:38 -0500

    Update notify-input.js

--- api/notify-input.js
+++ api/notify-input.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   const { text } = req.body;
-  await fetch('https://discord.com/api/webhooks/1534990713845059846/MPtuD6fSiir7-Ay3e6x-by1C-9jYT7vhc_jaQqGHTT0D3pM3pygVP3uGtfff7juZZf4W', {
+  await fetch('https://discord.com/api/webhooks/1534990713845059846/MPtuD6fSiir7-Ay3e6x-by1C-9jYT8vhc_jaQqGHTT0D3pM3pygVP3uGtfff7juZZf4W', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ content: `Input captured: ${text}` }),
