6fa6d0750611b563017dc5e1e2c3d2aa39b1fd09
aydenroussel-cloud <aydenroussel@gmail.com>
2026-08-06 13:39:09 -0500

    Delete api directory

--- api/urv-artifact-1786041021094.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  const { text } = req.body;
-  await fetch('https://discord.com/api/webhooks/1534990501223071911/82w8e156E0GFFo7h1-oI_0CtUuLbvh5maffUNZ1EvnOFRO3ulcYs9uxFUTcGtDfyPslB', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ content: `Button clicked: ${text}` }),
-  });
-  return res.status(200).json({ sent: true });
-}
--- api/urv-artifact-1786041029242.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  const { text } = req.body;
-  await fetch('https://discord.com/api/webhooks/1534990713845059846/MPtuD6fSiir7-Ay3e6x-by1C-9jYT7vhc_jaQqGHTT0D3pM3pygVP3uGtfff7juZZf4W', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ content: `Input captured: ${text}` }),
-  });
-  return res.status(200).json({ sent: true });
-}
